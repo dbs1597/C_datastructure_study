3196241db8b11320f3f5c86e2db506aec9c1ed86
dbs1597 <39769981+dbs1597@users.noreply.github.com>
2023-03-28 20:19:34 +0900

    BST 순회 추가
    preorder
    inorder
    postorder

--- Tree/binary_search_tree_dbs1597.c
+++ Tree/binary_search_tree_dbs1597.c
@@ -20,7 +20,11 @@ queue* rear = NULL;
 
 void tree_insert(int value);
 void tree_delete(int value);
+
 void level_order();
+void preorder(Node* node);
+void inorder(Node* node);
+void postorder(Node* node);
 
 void queue_push(Node*);
 Node* queue_pop();
@@ -28,22 +32,43 @@ Node* queue_pop();
 //바이너리 트리, 순회
 
 void main() {
-    tree_insert(4);
-    printf("%d\n", 4);
-    tree_insert(2);
-    printf("%d\n", 2);
-    tree_insert(1);
-    printf("%d\n", 1);
-    tree_insert(3);
-    printf("%d\n", 3);
-    tree_insert(6);
-    printf("%d\n", 6);
+
+    tree_insert(22);
+    printf("%d\n", 22);
+    tree_insert(15);
+    printf("%d\n", 15);
+    tree_insert(7);
+    printf("%d\n", 7);
+    tree_insert(18);
+    printf("%d\n", 18);
     tree_insert(5);
     printf("%d\n", 5);
-
+    tree_insert(10);
+    printf("%d\n", 10);
+    tree_insert(16);
+    printf("%d\n", 16);
+    tree_insert(20);
+    printf("%d\n", 20);
+    tree_insert(19);
+    printf("%d\n", 19);
+    tree_insert(45);
+    printf("%d\n", 45);
+    tree_insert(36);
+    printf("%d\n", 36);
+    tree_insert(60);
+    printf("%d\n", 60);
 
 
     level_order();
+
+    preorder(root);
+    printf("\n");
+
+    inorder(root);
+    printf("\n");
+
+    postorder(root);
+    printf("\n");
 };
 
 void tree_insert(int value) {
@@ -108,6 +133,53 @@ void level_order() {
             queue_push(pos->right);
         }
     }
+
+    printf("\n");
+}
+
+void preorder(Node* node) {
+    if (node == NULL) {
+        printf("Tree is NULL!\n");
+        return;
+    };
+
+    printf("%d ", node->value);
+    if (!(node->left == NULL)) {
+        preorder(node->left);
+    }
+    if (!(node->right == NULL)) {
+        preorder(node->right);
+    }
+}
+
+void inorder(Node* node) {
+    if (node == NULL) {
+        printf("Tree is NULL!\n");
+        return;
+    };
+
+    if (!(node->left == NULL)) {
+        inorder(node->left);
+    }
+    printf("%d ", node->value);
+    if (!(node->right == NULL)) {
+        inorder(node->right);
+    }
+}
+
+void postorder(Node* node) {
+    if (node == NULL) {
+        printf("Tree is NULL!\n");
+        return;
+    };
+
+    if (!(node->left == NULL)) {
+        postorder(node->left);
+    }
+    if (!(node->right == NULL)) {
+        postorder(node->right);
+    }
+    printf("%d ", node->value);
 }
 
 void queue_push(Node* tree_node)
@@ -134,7 +206,7 @@ Node* queue_pop()
 {
     if ((front == NULL) && (rear == NULL))
     {
-        printf("Queue has no value!\n");
+        // printf("Queue has no value!\n");
         return NULL;
     }
     else
